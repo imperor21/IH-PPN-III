bcbe182e76bcf4112c25ec9ce8a62da1ec9bd7a6
Samirah <47280862+imperor21@users.noreply.github.com>
2026-05-10 16:01:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 /* ✅ Pedoman PDF & Foto Dokumentasi → Google Drive (multi-device)    */
 /* ✅ IndexedDB dihapus — data terpusat di GAS/Drive                  */
 
-const API_URL = "https://script.google.com/macros/s/AKfycbwU8YjLgzBkf-hkqsZZt3Bp7yvwdfHik7dyqSTMnLu_xMDweuHkCi0a5ZQ5EYEbfamuXw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyqmQlhNRPQyf1h4n32yrkK6Y0F02wMlLgeIEF22IwP2mBaTM_5K9nsCijOdRSIU1Lmvg/exec";
 
 async function gasPost(payload) {
   const controller = new AbortController();
